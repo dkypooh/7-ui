52e11ca9b39d84b1b76335ff0f6d3b465d08bbd0
方波 <bluepooh@126.com>
2016-11-05 14:39:19 +0800

    server conf

---
+++ build/server.js
@@ -0,0 +1,14 @@
+/**
+ * server 打包脚本文件
+ *
+ * @author:   波比(｡･∀･)ﾉﾞ
+ * @date:     2016-11-05  下午1:38
+ */
+
+const EventEmmitter = require('event').eventEmmitter;
+
+class Server extends EventEmmitter{
+	constructor(){
+		
+	}
+}
